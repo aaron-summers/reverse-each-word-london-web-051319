1af1ad32227457afb95d750d552cfab633f0c22e
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 00:53:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ def reverse_each_word(string)
   end
   return new_string_array.join(" ")
 end
+
+def reverse_each_word(string)
+  
+  input_string = string.split
+  new_string_array = []
+  
+  input_string.collect do |word|
+    new_string_array << word.reverse
+  end
+  return new_string_array.join(" ")
+end
